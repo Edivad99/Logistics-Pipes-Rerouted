62caf0ae8b12c5dedf079f7f8d5fee7d5e02d4bd
Davide Albiero <davide2910@live.it>
2026-07-19 10:24:31 +0200

    Removed unused files

--- dummy/src/main/java/cpw/mods/ironchest/common/tileentity/chest/TileEntityIronChest.java
+++
@@ -1,7 +0,0 @@
-package cpw.mods.ironchest.common.tileentity.chest;
-
-import net.minecraft.world.level.block.entity.BlockEntity;
-
-public class TileEntityIronChest extends BlockEntity {
-
-}
--- dummy/src/main/java/ic2/api/classic/tile/machine/IProgressMachine.java
+++
@@ -1,6 +0,0 @@
-package ic2.api.classic.tile.machine;
-
-public interface IProgressMachine {
-	float getProgress();
-	float getMaxProgress();
-}
