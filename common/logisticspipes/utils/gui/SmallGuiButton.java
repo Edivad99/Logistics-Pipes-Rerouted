0a91e2bd48ab6ae045218bc851318526e6c70e5d
Davide Albiero <davide2910@live.it>
2026-07-28 20:47:36 +0200

    Update SmallGuiButton.java

--- common/logisticspipes/utils/gui/SmallGuiButton.java
+++ common/logisticspipes/utils/gui/SmallGuiButton.java
@@ -7,19 +7,25 @@
 
 package logisticspipes.utils.gui;
 
+import java.util.function.Consumer;
+
 import logisticspipes.utils.Color;
-import net.minecraft.client.Minecraft;
+
 import net.minecraft.client.gui.Font;
 import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.client.gui.components.AbstractButton;
 import net.minecraft.network.chat.Component;
+
+import lombok.Getter;
+import lombok.Setter;
 
 public class SmallGuiButton extends AbstractButton {
 
 	/** Replaces the old Button.id field removed in 1.20.1 */
 	public final int id;
 	private final int stringOffset;
-	private java.util.function.Consumer<SmallGuiButton> pressListener = b -> {};
+    @Setter
+	private Consumer<SmallGuiButton> pressListener = b -> {};
 
 	public SmallGuiButton(int buttonId, int x, int y, int width, int height, String label) {
 		this(buttonId, x, y, width, height, label, 0);
@@ -37,33 +43,27 @@ public class SmallGuiButton extends AbstractButton {
 		this.stringOffset = 0;
 	}
 
-	public void setPressListener(java.util.function.Consumer<SmallGuiButton> listener) {
-		this.pressListener = listener;
-	}
-
-	@Override
+    @Override
 	public void onPress() {
 		pressListener.accept(this);
 	}
 
-	@Override
-	public void renderWidget(GuiGraphics guiGraphics, int mouseX, int mouseY, float partialTicks) {
-		Font fontrenderer = Minecraft.getInstance().font;
-		boolean flag = isHovered();
-		// Background fill
-		int bg = !active ? 0xff444444 : flag ? 0xff888888 : 0xff666666;
-		guiGraphics.fill(getX(), getY(), getX() + width, getY() + height, bg);
-		// Simple beveled border: lighter top/left, darker bottom/right
-		guiGraphics.fill(getX(), getY(), getX() + width, getY() + 1, 0xffaaaaaa);
-		guiGraphics.fill(getX(), getY() + height - 1, getX() + width, getY() + height, 0xff333333);
-		guiGraphics.fill(getX(), getY(), getX() + 1, getY() + height, 0xffaaaaaa);
-		guiGraphics.fill(getX() + width - 1, getY(), getX() + width, getY() + height, 0xff333333);
-		// Label
-		int color = !active ? Color.getValue(Color.GREY) : flag ? Color.getValue(Color.LIGHT_YELLOW) : Color.getValue(Color.LIGHTER_GREY);
-		guiGraphics.drawCenteredString(fontrenderer, getMessage(), getX() + width / 2, getY() + (height - 8) / 2 + stringOffset, color);
-	}
+    @Override
+    public void renderString(GuiGraphics guiGraphics, Font font, int color) {
+        int minX = this.getX() + 2;
+        int maxX = this.getX() + this.getWidth() - 2;
+        renderScrollingString(guiGraphics, font, this.getMessage(), minX, this.getY() + stringOffset, maxX, this.getY() + this.getHeight() + stringOffset, color);
+    }
 
-	@Override
+    @Override
+    public int getFGColor() {
+        if (!active) {
+            return Color.getValue(Color.GREY);
+        }
+        return isHovered() ? Color.getValue(Color.LIGHT_YELLOW) : Color.getValue(Color.LIGHTER_GREY);
+    }
+
+    @Override
 	protected void updateWidgetNarration(net.minecraft.client.gui.narration.NarrationElementOutput narrationElementOutput) {
 		defaultButtonNarrationText(narrationElementOutput);
 	}
